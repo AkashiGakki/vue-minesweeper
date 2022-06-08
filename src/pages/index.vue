4d6980045742c43a3d6638b4536f92f01e2e366b
Akashi <akashi_sai@163.com>
2022-06-08 22:52:02 +0800

    :tada: chroe: canvas minesweeper

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,7 @@ interface BlockState {
   revealed?: boolean
   mine?: boolean
   flagged?: boolean
-  adjacentMines?: number
+  adjacentMines: number
 }
 
 const WIDTH = 10
@@ -13,36 +13,110 @@ const HEIGHT = 10
 
 const state = reactive(
   Array.from({ length: HEIGHT }, (_, y) =>
-    Array.from({ length: WIDTH }, (_, x) =>
-      y * 10 + x + 1,
+    Array.from({ length: WIDTH },
+      (_, x): BlockState => ({
+        x,
+        y,
+        adjacentMines: 0,
+      }),
+
     )),
 )
+
+function generateMines() {
+  state.forEach((row) => {
+    row.forEach((block) => {
+      block.mine = Math.random() < 0.3
+    })
+  })
+  // for (const row of state) {
+  //   for (const block of row)
+  //     block.mine = Math.random() < 0.1
+  // }
+}
+
+const directions = [
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+]
+
+const numberColors = [
+  'text-transparent',
+  'text-blue-500',
+  'text-green-500',
+  'text-yellow-500',
+  'text-red-500',
+  'text-purple-500',
+  'text-pink-500',
+  'text-teal-500',
+]
+
+function updateNumbers() {
+  state.forEach((row, y) => {
+    row.forEach((block, x) => {
+      if (block.mine)
+        return
+
+      directions.forEach(([dx, dy]) => {
+        const x2 = x + dx
+        const y2 = y + dy
+        if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
+          return
+
+        if (state[y2][x2].mine)
+          block.adjacentMines += 1
+      })
+    })
+  })
+}
 
 function onClick(x: number, y: number) {
   console.log(`x: ${x} y: ${y}`)
 }
+
+function getBlockClass(block: BlockState) {
+  return block.mine ? 'text-red' : numberColors[block.adjacentMines]
+}
+
+generateMines()
+updateNumbers()
 </script>
 
 <template>
   <div>
     Minesweeper
 
-    <div>
-      {{ data }}
-    </div>
-
-    <div v-for="row, y in state" :key="y">
-      <button
-        v-for="item, x in row"
-        :key="x"
-        w-10
-        h-10
-        border
-        hover:bg-gray
-        @click="onClick(x, y)"
+    <div p5>
+      <div
+        v-for="row, y in state"
+        :key="y"
+        flex="~"
+        items-center justify-center
       >
-        {{ item }}
-      </button>
+        <button
+          v-for="item, x in row"
+          :key="x"
+          flex="~"
+          items-center justify-center
+          w-10 h-10 m=".5"
+          border="1 gray-400/10"
+          hover="bg-gray/10"
+          :class="getBlockClass(item)"
+          @click="onClick(x, y)"
+        >
+          <!-- {{ item.mine ? 'x' : item.adjacentMines }} -->
+          <div v-if="item.mine" i-mdi-mine />
+          <div v-else>
+            {{ item.adjacentMines }}
+          </div>
+        </button>
+      </div>
     </div>
   </div>
 </template>
